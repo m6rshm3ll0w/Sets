da10d1e7d32bdf0017d3c8b9e39b5c30227aa8fd
m6rshm3ll0w <mihail.gm.kms@gmail.com>
2023-12-26 19:06:17 +0300

    method search pares was upped and optimazed

--- Set.py
+++ Set.py
@@ -15,23 +15,11 @@ tabs_c = ["|_#_|___|___|\n|___|___|___|\n|___|___|___|\n|___|___|___|",
           "|___|___|___|\n|___|___|___|\n|___|___|___|\n|___|___|_#_|"
           ]
 n = 0
-lbd = []
-while n <= 11:
-    si = tabs_c[n]
+lbd = ['3VO1', '1VR1', '3GR1',
+       '3GR3', '1GS1', '2VS3',
+       '2RR1', '3GS2', '3RO1',
+       '1GR3', '2RO2', '2GS2']
 
-    print("Введи значение карточки согласно образцу, снизу указано расположение карточки ⇓")
-    print(si, "\n")
-    sel_tab = input("Ex: 2RO1 \n "
-                    "# 2 - количество фигур (1, 2, 3) \n "
-                    "# R - цвет рисунка (R - красный, V - фиолетовый, G - зелёный) \n "
-                    "# O - форма(O - круг, S - волна, R - ромб)\n "
-                    "# 1 - окрас(1 - закрашено, 2 - штрих, 3 - нет заливки) \n "
-                    "# : ")
-    lbd.append(sel_tab)
-    n += 1
-    print("Ok :)")
-    time.sleep(0.8)
-    os.system('CLS')
 
 def reload(str):
     lista = []
@@ -40,7 +28,74 @@ def reload(str):
     return lista
 
 
-def dont_false(chars_f,chars_s,chars_t):
+# search
+
+
+def select_pare(lbd):
+    i = 0
+    lists = []
+    print("‖ Генерация пар ...")
+
+    while i <= 11:
+
+        newCard = lbd.copy()  # создаем копию введеного поля
+        card1 = lbd[i]  # выбираем 1 карту по порядку 1 --- 12
+        newCard.remove(card1)  # из копии поля удаляем выбранную карту
+
+
+        for x in newCard:  # первый Элем...
+            # print(f"‖ ‖ создаем {dnf}/11 пару для {i + 1}/12 карт")
+            sefa = [card1, x]
+            # print(f"‖ ‖⋙ пара {sefa}")
+            lists.append(sefa)
+            # print(f"‖ ‖                  {card1} {x}")
+        print(f"‖ ‖ {i} пара с карточкой {card1} готова! [x11]")
+        # print(f"‖ ‖  пары с карточкой {card1} готовы!\n"
+        #       f"‖ ‖‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗\n"
+        #       f"‖ ‖")
+        i += 1
+
+    i = 0
+    clear_list = lists.copy()
+    print("‖ Удаление повторяющихся пар ...")
+    while i <= 65:
+
+        t_pare = clear_list[i]
+        fe, se = t_pare[0], t_pare[1]
+        t_pare = [fe, se]
+        clear_list.remove(t_pare)
+        i += 1
+
+    print(f"‖‗ В итоге мы получили {len(clear_list)} пар ...\n")
+    return clear_list
+
+
+def manage_set(pare_set):
+    i = 0
+    print("‖ select set")
+    lists = []  # сеты
+
+    while i <= 65:
+
+        newCard = lbd.copy()  # список доступных карт
+        card_pare = pare_set[i]
+        card1 = card_pare[0]
+        card2 = card_pare[1]
+        newCard.remove(card1)
+        newCard.remove(card2)
+
+        for x in newCard:  # первый Элем...
+            sefa = [card1, card2, x]
+            lists.append(sefa)
+        i += 1
+        print(f"‖ ‖ {i} сет с карточками {card1} {card2} готов! [x10]")
+    print(f"‖‗ В итоге мы получили {len(lists)} пар ...\n")
+
+
+    return lists
+
+
+def dont_false(chars_f, chars_s, chars_t):
     if chars_f != chars_s and chars_f != chars_t and chars_s != chars_f and chars_s != chars_t and chars_t != chars_f and chars_t != chars_s:
         return "False"
     if chars_f == chars_s == chars_t:
@@ -68,7 +123,7 @@ def comparison(selected_set):
     bright_s = S_e[3]
     bright_t = T_e[3]
 
-    if "True" == dont_false(chars_f, chars_s, chars_t):# одинаковое количество фигур
+    if "True" == dont_false(chars_f, chars_s, chars_t):  # одинаковое количество фигур
         if "True" == dont_false(color_f, color_s, color_t):  #
             if "True" == dont_false(form_f, form_s, form_t):
                 if "True" == dont_false(bright_f, bright_s, bright_t):
@@ -146,48 +201,8 @@ def comparison(selected_set):
         else:
             return "False"
     else:
+        # print("is dont set")
         return "False"
-        print("isdont")
-
-
-def setect_pare(lbd):
-    i = 0
-    lists = []
-    while i <= 11:
-        newCard = lbd.copy()
-        card1 = lbd[i]
-        newCard.remove(card1)
-        dnf = 1
-        for x in newCard:  # первый Элем...
-            sefa = [card1, x]
-            lists.append(sefa)
-            dnf += 1
-        i += 1
-
-    return lists
-
-
-def manage_set(pare_set):
-    i = 0
-    lists = []
-    while i <= 11:
-        newCard = lbd.copy()
-        cardsek1 = pare_set[i]
-        card1 = cardsek1[0]
-        card2 = cardsek1[1]
-
-        newCard.remove(card1)
-        newCard.remove(card2)
-
-        dnf = 1
-        for x in newCard:  # первый Элем...
-            sefa = [card1, card2, x]
-            lists.append(sefa)
-            dnf += 1
-        i += 1
-
-    print("‖‗Done!\n")
-    return lists
 
 
 def search_sets(set_variat):
@@ -206,43 +221,59 @@ def search_sets(set_variat):
     return listseww
 
 
-def print_set_sh(list_set_view,lbd):
+def print_set_sh(list_set, lbd):
     i = 1
-    i = int(i)
+    # i = int(i)
     list = []
-    print(f"вы ввели\n"
-          f"|{lbd[0]}|{lbd[1]}|{lbd[2]}|\n"
-          f"|{lbd[3]}|{lbd[4]}|{lbd[5]}|\n"
-          f"|{lbd[6]}|{lbd[7]}|{lbd[8]}|\n"
-          f"|{lbd[9]}|{lbd[10]}|{lbd[11]}|\n"
+    print(f"\nBы ввели: \n\n"
+          f"| {lbd[0]} | {lbd[1]} | {lbd[2]} |\n"
+          f"| {lbd[3]} | {lbd[4]} | {lbd[5]} |\n"
+          f"| {lbd[6]} | {lbd[7]} | {lbd[8]} |\n"
+          f"| {lbd[9]} | {lbd[10]} | {lbd[11]} |\n\n"
           f"из этих карточек можно собрать такие сеты:")
     exit_print_set = ""
     set_numb = 0
-    lisset = None
-    while i <= len(list_set_view):
+    while i <= len(list_set):
 
-        set_wr = list_set_view[0]
+        set_wr = list_set[0]
         for x in set_wr:
             list.append(lbd.index(x))
         list.sort()
-        if list == lisset:
-            break
         print(f"cет № {i}")
-        lisset = list
         while set_numb <= 11:
             if set_numb == list[0] or set_numb == list[1] or set_numb == list[2]:
                 exit_print_set = exit_print_set + "|_#_|"
             else:
                 exit_print_set = exit_print_set + "|___|"
-            if set_numb == 2 or set_numb == 5 or set_numb == 8:
+            if set_numb == 2 or set_numb == 5 or set_numb == 8 or set_numb == 11:
                 exit_print_set = exit_print_set + "\n"
             set_numb += 1
         print(exit_print_set)
         i = i + 1
-print("‖ Making pares ...")
-pare_set = setect_pare(lbd)
-set_variations = manage_set(pare_set)
-list_set_view = search_sets(set_variations)
-print_set_sh(list_set_view,lbd)
 
-input("to Exit code press Enter")
+
+# while n <= 11:
+#     si = tabs_c[n]
+#
+#     print("Введи значение карточки согласно образцу, снизу указано расположение карточки ⇓")
+#     print(si, "\n")
+#     sel_tab = input("Ex: 2RO1 \n "
+#                     "# 2 - количество фигур (1, 2, 3) \n "
+#                     "# R - цвет рисунка (R - красный, V - фиолетовый, G - зелёный) \n "
+#                     "# O - форма(O - круг, S - волна, R - ромб)\n "
+#                     "# 1 - окрас(1 - закрашено, 2 - штрих, 3 - нет заливки) \n "
+#                     "# : ")
+#     n += 1
+#
+#     print("Ok :)")
+#     time.sleep(0.8)
+#     os.system('CLS')
+#     lbd.append(sel_tab)
+
+
+pare_set = select_pare(lbd) # documented
+set_variations = manage_set(pare_set) #
+list_set = search_sets(set_variations) #
+print_set_sh(list_set, lbd) # ok
+
+input("To exit code press enter")
